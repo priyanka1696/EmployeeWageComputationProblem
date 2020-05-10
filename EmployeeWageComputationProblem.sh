b2c5149fba7bbde6f77012d073a7955ac7bd7311
Priyanka <fulmogarepriya@gmail.com>
2020-05-10 22:22:01 +0530

    Total Working Hours For Month

--- EmployeeWageComputationProblem.sh
+++ EmployeeWageComputationProblem.sh
@@ -3,27 +3,30 @@ echo "Welcome to Employee Wage Computation Program"
 
 PartTime=1
 FullTime=2
-totalSalary=0;
+totalHrs=0;
+TotalWorkingDays=0;
+HrsInMonth=100;
 empRatePerHrs=20;
 NumberOfWorkingDays=20;
 
-for(( Day=1; Day<=$NumberOfWorkingDays; Day++ ))
+while [[ $totalHrs -lt $HrsInMonth && $TotalWorkingDays -lt $NumberOfWorkingDays ]]
 do
-	randomCheck=$(($RANDOM%2));
-	case $randomCheck in
-		$FullTime)
-			empHrs=8
-			;;
-		$PartTime)
-			empHrs=4
-			;;
-		*)
-			empHrs=0
-			;;
-	esac
-
-salary=$(($empHrs*$empRatePerHrs));
-totalSalary=$(($totalSalary+$salary))
-
+	((TotalWorkingDays++))
+	randomCheck=$(($RANDOM%3));
+		case $randomCheck in
+			$FullTime)
+				empHrs=8
+				;;
+			$PartTime)
+				empHrs=4
+				;;
+			*)
+				empHrs=0
+				;;
+		esac
+		totalHrs=$(($totalHrs+$empHrs));
 done
-echo "Employee Salary " $totalSalary;
+totalSalary=$(($totalHrs*$empRatePerHrs));
+echo "Employee Work Hour "$totalHrs
+echo "Employee Salary "$totalSalary
+
