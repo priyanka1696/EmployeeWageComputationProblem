6449ede43a43b857a15ec59c7f3bba115cdac52a
Priyanka <fulmogarepriya@gmail.com>
2020-05-10 20:49:48 +0530

    Calculate Daily Employee Wage

--- EmployeeWageComputationProblem.sh
+++ EmployeeWageComputationProblem.sh
@@ -7,8 +7,13 @@ randomCheck=$(($RANDOM%2));
 
 if [ $isPresent -eq $randomCheck ]
 then
+	empRatePerHrs=20;
+	empHrs=8;
+	salary=$(($empHrs*$empRatePerHrs));
         echo "Employee Present";
+	echo "Employee Daily Salary is "$salary;
 else
-        echo "Employee Not Present";
+	salary=0;
+        echo "Employee absent";
 fi
 
